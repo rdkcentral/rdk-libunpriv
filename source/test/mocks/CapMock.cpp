ecc5fcec9ba06d415ca5d02d6a424adb0ba30839
MonekaLakshmi <101797473+MonekaLakshmi@users.noreply.github.com>
2025-11-28 10:16:29 +0530

    RDKEMW-10008: Update CapMock.cpp

--- source/test/mocks/CapMock.cpp
+++ source/test/mocks/CapMock.cpp
@@ -4,7 +4,7 @@
 *
 * Copyright 2020 RDK Management
 *
-* Licensed under the Apache License, Version 2.0 (the "License");
+* SPDX-License-Identifier: Apache-2.0
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 *
