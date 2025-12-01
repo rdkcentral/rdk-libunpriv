a60ad428971af9b2eeb16773a5bdfce3cd32311f
MonekaLakshmi <101797473+MonekaLakshmi@users.noreply.github.com>
2025-12-01 11:39:32 +0530

    RDKEMW-10008: Update run_ut.sh

--- test/run_ut.sh
+++ test/run_ut.sh
@@ -4,16 +4,10 @@ SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 TOP_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
 cd "$TOP_DIR" || exit 1
 
-ENABLE_COV=false
-fail=0
-
-if [ "$1" = "--enable-cov" ]; then
-    echo "Enabling coverage options"
-    export CXXFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
-    export CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
-    export LDFLAGS="-lgcov --coverage"
-    ENABLE_COV=true
-fi
+echo "Enabling coverage options"
+export CXXFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
+export CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
+export LDFLAGS="-lgcov --coverage"
 
 autoreconf --install
 
@@ -33,12 +27,11 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-if [ "$ENABLE_COV" = true ]; then
-    echo "Generating coverage report"
-    lcov --capture --directory .. --output-file coverage.info
-    lcov --remove coverage.info '/usr/*' '*gmocks*' '*test*' --output-file coverage.info
-    lcov --list coverage.info
-fi
+echo "Generating coverage report"
+lcov --capture --directory .. --output-file coverage.info
+lcov --remove coverage.info '/usr/*' '*gmocks*' '*test*' --output-file coverage.info
+lcov --list coverage.info
+genhtml coverage.info --output-directory out 
 
 if [ $fail -ne 0 ]; then
     echo "Some unit tests failed."
